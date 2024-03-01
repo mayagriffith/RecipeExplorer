12313136caf81f2fb5d27a0913f9a5a56d15f2dc
Maya Griffith <mayagriffith@ip0af5c19d.int.uvm.edu>
2024-02-29 19:05:33 -0500

    got alcoholic drinks out

--- cleaning_data.py
+++ cleaning_data.py
@@ -1,8 +1,8 @@
 import zipfile
 import pandas as pd
 
-zip_file = 'RecipeData.zip'  # Replace with the correct path
-csv_file = 'epi_r.csv'  # Replace with the correct file name if different
+zip_file = 'RecipeData.zip' 
+csv_file = 'epi_r.csv' 
 
 # Unzipping the data
 with zipfile.ZipFile(zip_file, 'r') as zip_ref:
@@ -12,17 +12,13 @@ with zipfile.ZipFile(zip_file, 'r') as zip_ref:
 data_path = 'RecipeData/' + csv_file
 df = pd.read_csv(data_path)
 
-df_alcoholic = df[df['alcoholic'] == True]
-print(df_alcoholic)
-alcoholic_keywords = ['margarita', 'bloody mary', 'martini', 'cocktail', 'beer', 'wine', 'vodka', 'whiskey']
-df_non_alcoholic = df[~df['title'].str.lower().str.contains('|'.join(alcoholic_keywords))]
-
+df_clean = df[df['alcoholic'] == False]
 columns = df.columns.tolist()
 print(columns)
 
 
 print('size of regular df', df.shape[0])
-print('size of cleaned df', df_non_alcoholic.shape[0])
+print('size of cleaned df', df_clean.shape[0])
 
 # Data Exploration
 # print(df.head())
