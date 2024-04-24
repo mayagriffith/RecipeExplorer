6548d5d0751d90dfa3f50f4be81b1bb7473294bb
NickNBos <nnbosley@uvm.edu>
2024-04-24 16:06:41 -0400

    More accurate to what I intended
    but a bit uncontrolled

--- tree_search.py
+++ tree_search.py
@@ -67,7 +67,7 @@ def get_cleaned_recipes(tag_list):
 
 def create_model(features_df, tags_df):
     # classifier = RandomForestClassifier(random_state=0)
-    classifier = DecisionTreeClassifier(random_state=0, max_leaf_nodes=100)
+    classifier = DecisionTreeClassifier(random_state=0)
     
     
     x_train, x_test, y_train, y_test = train_test_split(features_df, tags_df, test_size=0.25)
@@ -85,51 +85,53 @@ if __name__ == "__main__":
     tag_list = []
     # tag_list.append('gourmet')
     
-    # tag_list.append('side')
-    # tag_list.append('dinner')
-    # tag_list.append('lunch')
-    # tag_list.append('dessert')
-    # tag_list.append('appetizer')
+    tag_list.append('side')
+    tag_list.append('dinner')
+    tag_list.append('lunch')
+    tag_list.append('dessert')
+    tag_list.append('appetizer')
     
     tag_list.append('chicken')
     tag_list.append('fish')
-    # tag_list.append('cod')
-    # tag_list.append('salmon')
-    # tag_list.append('beef')
-    # tag_list.append('beef rib')
-    # tag_list.append('bean')
+    tag_list.append('cod')
+    tag_list.append('salmon')
+    tag_list.append('beef')
+    tag_list.append('beef rib')
+    tag_list.append('bean')
     
     
-    # # tag_list.append('bread')
-    # tag_list.append('pasta')
-    # tag_list.append('cake')
+    # tag_list.append('bread')
+    tag_list.append('pasta')
+    tag_list.append('cake')
     
 
-    # # tag_list.append('tomato')
-    # # tag_list.append('onion')
-    # tag_list.append('potato')   
-    # # tag_list.append('turnip')
+    # tag_list.append('tomato')
+    # tag_list.append('onion')
+    tag_list.append('potato')   
+    # tag_list.append('turnip')
     
     
-    # # tag_list.append('low cal')
-    # tag_list.append('lettuce')
-    # tag_list.append('spinach')
-    # tag_list.append('bell pepper')
+    # tag_list.append('low cal')
+    tag_list.append('lettuce')
+    tag_list.append('spinach')
+    tag_list.append('bell pepper')
     
     
+    forest = []
     
     
     cleaned_recipes_df, tags_df = get_cleaned_recipes(tag_list)
     
-    # Implement the ratios later? Or maybe not at all
-    # macro_ratios_df = get_macro_ratios(cleaned_recipes_df)
-    # print(create_model(cleaned_recipes_df.iloc[:,1:], tags_df))
-    classifier, score = create_model(cleaned_recipes_df.iloc[:,1:], tags_df)
-    print(score)
-    
-    # cleaned_recipes_df['label'] = kmean.labels_ # This will give which recipes belong to each label
-    
-    plot_tree(classifier)
+    for tag in tag_list:
+        tag_series = tags_df[tag]
+        # macro_ratios_df = get_macro_ratios(cleaned_recipes_df)
+        # print(create_model(cleaned_recipes_df.iloc[:,1:], tags_df))
+        classifier, score = create_model(cleaned_recipes_df.iloc[:,1:], tag_series)
+        print(tag, score)
+        
+        # cleaned_recipes_df['label'] = kmean.labels_ # This will give which recipes belong to each label
+        forest.append(classifier)
+
     #%%
     
     # Get user input
@@ -149,19 +151,26 @@ if __name__ == "__main__":
     user_input_df['sodium'] = [sodium_goal]
     user_input_df['carb'] = [carb_goal]
     
-    
-    # Extract the label from the prediction
-    user_input_label = classifier.predict(user_input_df)[0]
-    user_input_label = [bool(value) for value in user_input_label]
+    user_labels = []
+    user_tags = []
+    for tag_index, classifier in enumerate(forest):
+        # Extract the label from the prediction
+        user_input_label = classifier.predict(user_input_df)[0]
+        user_input_label = user_input_label == 1
+        user_labels.append(user_input_label)
+        if user_input_label:
+            user_tags.append(tag_list[tag_index])
+        
     print('\n\n\n')
-    print('Tags:', np.asarray(tag_list)[user_input_label])
+    print('Tags:', user_tags)
+    
     # make it so that all columns are displayed so protein and fat can be viewed easily
     pd.set_option('display.max_columns', None)
     
     # Likely a better way than this, but this isn't that hard
     recipe_match = []
     for i in range(len(tags_df)):
-        recipe_match.append((tags_df.iloc[i,:] == user_input_label).all())
+        recipe_match.append((tags_df.iloc[i,:] == user_labels).all())
     
     print(cleaned_recipes_df[recipe_match].head())
     
